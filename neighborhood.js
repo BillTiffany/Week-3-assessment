1648c557449ddff39c8c59790c8eed8f825d20ce
Bill Tiffany <daganlegalservice@gmail.com>
2021-10-01 14:41:05 -0600

    updating

--- neighborhood.js
+++ neighborhood.js
@@ -4,8 +4,16 @@ function handleSubmit(evt) {
 	form.addEventListener('submit', () => {
 		alert('Thanks for the message, we now have access to your sensitive files');
 	  });;
-}
+    }
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+document.getElementById("myVid").addEventListener("mouseover", function() {
+	this.play();
+});
+
+document.getElementById("myVid").addEventListener("mouseleave", function() {
+	this.pause();
+});
