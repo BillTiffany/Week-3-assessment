e6611ca116884ddd91a8e6a9e5b3daa9fc7f79a5
Bill Tiffany <daganlegalservice@gmail.com>
2021-10-07 14:13:45 -0600

    updating for Kyle

--- neighborhood.js
+++ neighborhood.js
@@ -10,10 +10,31 @@ let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
 
+let newRest = document.querySelector(".restform")
 
+const array = ["Uncle Moe's Family Feedbag", "The Frying Dutchman", 
+"Up, Up and Buffet", "Wall E Weasels", "Two Guys From Kabul", 
+"Pretzel Wagon", "Planet Hype", "Greaser's Cafe", "You Thai Now", 
+"Municipal House of Pancakes","Malaria Zone","Leaning Tower of Pizza", 
+"Le Krusty Burger", "Spruce Caboose"];
 
-const months = ["January", "February", "March", "April", "May", "June", "July"];
+function getRandomItem(arr) {
 
-const random = Math.floor(Math.random() * months.length);
-console.log(random, months[random]);
+    // get random index value
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    // get random item
+    const item = arr[randomIndex];
+    
+    const simps = document.createElement('h3')
+    simps.innerText = randomIndex
+    let section = document.querySelector(".restBtn")
+    section.appendChild(simps)
+}
 
+const asdf = document.querySelector('.restaraunt')
+asdf.addEventListener('click', getRandomItem)
+
+const result = getRandomItem(array);
+console.log(result);
+
+document.createElement()
